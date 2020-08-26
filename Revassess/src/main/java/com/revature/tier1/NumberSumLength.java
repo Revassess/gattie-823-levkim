a6016b1c23c493b333a42065c2e7f109958c11c2
Lev Kim <irene.kim@revature.net>
2020-08-26 15:08:34 -0400

    successfully tested 3 out of 4

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,19 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		if (num != 0) {
+			int result = 0; 
+			int length = Long.toString(num).length();
+			
+			for (int i = 0; i < length; ++i) {
+				int n = Long.toString(num).indexOf(i);
+				result += (n * n);
+			}
+			
+			if (result == num) {
+				return true;
+			}
+		}
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		if (arr != null) {
+			int total = 0;
+			for (int i = 0; i < arr.length; ++i) {
+				int item = arr[i];
+				total += item;
+			}
+			return total;
+		} else {
+			return 0;
+		}
 	}
 }
